ba4ae63594ef2e728248ff5c7f9bb37a25da41ee
Narasimha Swamy Matta (DHL IT Services) <narasimha_swamy.matta@dhl.com>
2021-06-29 23:10:02 +0800

    Updated file

--- src/main/java/com/tiagoamp/productcatalog/ProductCatalogApplication.java
+++ src/main/java/com/tiagoamp/productcatalog/ProductCatalogApplication.java
@@ -25,41 +25,30 @@ public class ProductCatalogApplication {
 	ApplicationRunner applicationRunner(Environment environment) {
 		return args -> {
 			
-			System.out.println("NARA RUNNNING DURING STARTUP");
-			System.out.println("NARA RUNNNING DURING STARTUP");
-			System.out.println("NARA RUNNNING DURING STARTUP");
-			System.out.println("NARA RUNNNING DURING STARTUP");
-			InputStream inputStream = null;
-			try {
-				Properties prop = new Properties();
-				String propFileName = "ESBFluentd.properties";
-
-				inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-				if (inputStream != null) {
-					prop.load(inputStream);
-				} else {
-					throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-				}
-				
-				FileOutputStream out = new FileOutputStream("ESBFluentd.properties");
-				prop.setProperty("FluentdEnv", "DEV");
-				prop.setProperty("FluentdHostList", "LOCALHOST");
-				prop.store(out, null);
-				out.close();
-				
-				
-
-			} catch (Exception e) {
-				System.out.println("Exception: " + e);
-			} finally {
-				try {
-					if (inputStream != null)
-						inputStream.close();
-				} catch (IOException e) {
-					System.out.println("Exception: " + e);
-				}
-			}
+			/*
+			 * System.out.println("NARA RUNNNING DURING STARTUP");
+			 * System.out.println("NARA RUNNNING DURING STARTUP");
+			 * System.out.println("NARA RUNNNING DURING STARTUP");
+			 * System.out.println("NARA RUNNNING DURING STARTUP"); InputStream inputStream =
+			 * null; try { Properties prop = new Properties(); String propFileName =
+			 * "ESBFluentd.properties";
+			 * 
+			 * inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+			 * 
+			 * if (inputStream != null) { prop.load(inputStream); } else { throw new
+			 * FileNotFoundException("property file '" + propFileName +
+			 * "' not found in the classpath"); }
+			 * 
+			 * FileOutputStream out = new FileOutputStream("ESBFluentd.properties");
+			 * prop.setProperty("FluentdEnv", "DEV"); prop.setProperty("FluentdHostList",
+			 * "LOCALHOST"); prop.store(out, null); out.close();
+			 * 
+			 * 
+			 * 
+			 * } catch (Exception e) { System.out.println("Exception: " + e); } finally {
+			 * try { if (inputStream != null) inputStream.close(); } catch (IOException e) {
+			 * System.out.println("Exception: " + e); } }
+			 */
 
 		};
 	}
